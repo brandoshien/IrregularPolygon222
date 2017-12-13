4e7f142bb0d3e98e1583706e9fc41b54454b844f
Brandon Shien <bs2897@pleasantonusd.net>
2017-12-12 22:13:08 -0800

    Changed formatting
    changed formatting

--- IrregularPolygon.java
+++ IrregularPolygon.java
@@ -1,7 +1,8 @@
 
 import java.awt.geom.*;     // for Point2D.Double
 import java.util.ArrayList; // for ArrayList
-import gpdraw.*;            // for DrawingTool
+import gpdraw.*; 
+           // for DrawingTool
 
 public class IrregularPolygon{
     private ArrayList <Point2D.Double> myPolygon = new ArrayList<>();
